00ff7edc3289dc753a76ed69c6a306cdd104574c
Andres Cataneo <88565796+AndresCataneo@users.noreply.github.com>
2025-08-27 00:15:51 -0600

    clientMulti.c

--- clientMulti.c
+++ clientMulti.c
@@ -27,47 +27,53 @@ int main(int argc, char *argv[]) {
     int num_files = 0;
     int shift = 0;
 
-    // Parsear argumentos
-    for (int i = 2; i < argc; i++) {
+    // Parsear argumentos - encontrar dónde terminan los puertos
+    int i;
+    for (i = 2; i < argc; i++) {
         if (is_number(argv[i]) && num_ports < 3) {
             ports[num_ports++] = atoi(argv[i]);
         } else {
-            // El último argumento debe ser el shift
-            if (i == argc - 1 && is_number(argv[i])) {
-                shift = atoi(argv[i]);
-                break;
-            }
-            // Los argumentos restantes son archivos
-            num_files = argc - i - 1;
             break;
         }
     }
 
+    // El último argumento debe ser el shift
+    if (i < argc && is_number(argv[argc - 1])) {
+        shift = atoi(argv[argc - 1]);
+        // Los archivos están entre los puertos y el shift
+        num_files = argc - 1 - i;
+    } else {
+        printf("Error: Shift value missing or invalid\n");
+        exit(1);
+    }
+
     if (num_ports == 0 || num_files == 0 || shift == 0) {
         printf("Error: Invalid arguments\n");
+        printf("Ports found: %d, Files found: %d, Shift: %d\n", num_ports, num_files, shift);
         printf("USE: %s <SERVER_IP> <PORT1> [PORT2] [PORT3] <FILE1> [FILE2] [FILE3] <SHIFT>\n", argv[0]);
         exit(1);
     }
 
-    char **files = &argv[2 + num_ports];
+    char **files = &argv[i];
     
     printf("Connecting to server %s\n", server_ip);
     printf("Ports: ");
-    for (int i = 0; i < num_ports; i++) printf("%d ", ports[i]);
+    for (int j = 0; j < num_ports; j++) printf("%d ", ports[j]);
     printf("\nFiles: ");
-    for (int i = 0; i < num_files; i++) printf("%s ", files[i]);
+    for (int j = 0; j < num_files; j++) printf("%s ", files[j]);
     printf("\nShift: %d\n", shift);
 
     // Procesar cada puerto/archivo
-    for (int i = 0; i < num_ports && i < num_files; i++) {
-        FILE *fp = fopen(files[i], "r");
+    for (int j = 0; j < num_ports && j < num_files; j++) {
+        FILE *fp = fopen(files[j], "r");
         if (!fp) {
             perror("Error opening file");
             continue;
         }
 
         char file_content[BUFFER_SIZE] = {0};
-        fread(file_content, 1, sizeof(file_content) - 1, fp);
+        size_t bytes_read = fread(file_content, 1, sizeof(file_content) - 1, fp);
+        file_content[bytes_read] = '\0';
         fclose(fp);
 
         int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -78,10 +84,10 @@ int main(int argc, char *argv[]) {
 
         struct sockaddr_in serv_addr;
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(ports[i]);
+        serv_addr.sin_port = htons(ports[j]);
         inet_pton(AF_INET, server_ip, &serv_addr.sin_addr);
 
-        printf("Connecting to port %d...\n", ports[i]);
+        printf("Connecting to port %d...\n", ports[j]);
         if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
             perror("Connection failed");
             close(sock);
@@ -89,7 +95,7 @@ int main(int argc, char *argv[]) {
         }
 
         char buffer[BUFFER_SIZE];
-        snprintf(buffer, sizeof(buffer), "%d|%d|%s", ports[i], shift, file_content);
+        snprintf(buffer, sizeof(buffer), "%d|%d|%s", ports[j], shift, file_content);
         
         if (send(sock, buffer, strlen(buffer), 0) < 0) {
             perror("Send failed");
@@ -101,9 +107,9 @@ int main(int argc, char *argv[]) {
         int bytes = recv(sock, response, sizeof(response) - 1, 0);
         if (bytes > 0) {
             response[bytes] = '\0';
-            printf("[PORT %d] SERVER RESPONSE: %s\n", ports[i], response);
+            printf("[PORT %d] SERVER RESPONSE: %s\n", ports[j], response);
         } else {
-            printf("[PORT %d] No response from server\n", ports[i]);
+            printf("[PORT %d] No response from server\n", ports[j]);
         }
         
         close(sock);
